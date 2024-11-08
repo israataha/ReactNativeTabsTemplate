990c601543d8bc06bedc7d7d91af2e6b76eb25da
israataha <israataha@gmail.com>
2024-11-08 14:27:53 -0600

    Adjust App.test.tsx imports

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,8 @@
-/**
- * @format
- */
-
-import 'react-native';
-
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+import { render } from '@testing-library/react-native';
 import React from 'react';
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 import App from '../App';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  render(<App />);
 });
