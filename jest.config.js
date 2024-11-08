ee39818ecad5879716203d8c522f95c55e5ad50f
israataha <israataha@gmail.com>
2024-11-07 21:10:45 -0600

    Configure jest test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverageFrom: ['**/*.{ts,tsx}'],
+  coverageReporters: ['json-summary', ['text', { file: 'coverage.txt' }]],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'coverage',
+        outputName: 'jest-junit.xml',
+        ancestorSeparator: ' › ',
+        uniqueOutputName: 'false',
+      },
+    ],
+  ],
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
   transformIgnorePatterns: [
